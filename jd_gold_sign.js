b712c392ff8f6d5a7fc2a8f1c4396147d7ccaae5
kenji <941196393@qq.com>
2022-02-07 11:13:57 +0800

    update

--- jd_gold_sign.js
+++ jd_gold_sign.js
@@ -30,7 +30,6 @@ if ($.isNode()) {
         ...$.toObj($.getdata("CookiesJD") || "[]").map((item) => item.cookie)].filter((item) => !!item);
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-
 !(async () => {
     if (!cookiesArr[0]) {
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', { "open-url": "https://bean.m.jd.com/" });
